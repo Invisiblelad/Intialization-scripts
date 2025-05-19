48f18ee5308812967c6aafd09e154e4289b3388e
Sharath Veerapaneni <140253698+Invisiblelad@users.noreply.github.com>
2025-05-19 22:02:47 +0530

    Update worker-script.sh

--- worker-script.sh
+++ worker-script.sh
@@ -1,12 +1,26 @@
-!#/bin/bash
+#!/bin/bash
 
-apt-get update
-apt-get install docker.io
+set -e  
+
+# Update and install prerequisites
+apt-get update -y
+apt-get install -y docker.io curl apt-transport-https gnupg lsb-release
+
+# Enable and start Docker
+systemctl enable docker
 systemctl start docker
 
-apt-get install curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt-get install kubeadm kubelet kubectl
+# Add Kubernetes apt repository
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+# Install kubelet, kubeadm, kubectl
+apt-get update -y
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+# Disable swap
 swapoff -a
+sed -i '/ swap / s/^/#/' /etc/fstab
+
 hostnamectl set-hostname worker
